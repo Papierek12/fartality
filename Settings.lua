daf7197ebd1dfb98c9e25b3f9f1840b200b50b9c
Papierek12 <151555097+Papierek12@users.noreply.github.com>
2026-01-27 13:36:32 +0100

    Create Settings.lua

---
+++ Settings.lua
@@ -0,0 +1,35 @@
+getgenv().ExunysDeveloperAimbot = {
+    DeveloperSettings = {
+        UpdateMode = "RenderStepped",
+        TeamCheckOption = "TeamColor",
+        RainbowSpeed = 1
+    },
+    Settings = {
+        Enabled = false,
+        TeamCheck = false,
+        AliveCheck = true,
+        WallCheck = true,
+        OffsetToMoveDirection = false,
+        OffsetIncrement = 15,
+        Sensitivity = 0.1,
+        Sensitivity2 = 3.5,
+        LockMode = 1,
+        LockPart = "Head",
+        TriggerKey = Enum.UserInputType.MouseButton2,
+        Toggle = false
+    },
+    FOVSettings = {
+        Enabled = true,
+        Visible = true,
+        Radius = 90,
+        NumSides = 60,
+        Thickness = 1,
+        Transparency = 1,
+        Filled = false,
+        RainbowColor = false,
+        RainbowOutlineColor = false,
+        Color = Color3.fromRGB(255, 255, 255),
+        OutlineColor = Color3.fromRGB(0, 0, 0),
+        LockedColor = Color3.fromRGB(255, 150, 150)
+    }
+}
